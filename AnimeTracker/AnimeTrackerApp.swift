79e20349d5a699dd791accee128e7569d90edad0
Taha Rashid <taharashid710192@gmail.com>
2024-10-17 15:54:33 -0400

    Initial Commit

---
+++ AnimeTracker/AnimeTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimeTrackerApp.swift
+//  AnimeTracker
+//
+//  Created by Taha Rashid on 2024-10-17.
+//
+
+import SwiftUI
+
+@main
+struct AnimeTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AnimeTrackerTests/AnimeTrackerTests.swift
@@ -0,0 +1,17 @@
+//
+//  AnimeTrackerTests.swift
+//  AnimeTrackerTests
+//
+//  Created by Taha Rashid on 2024-10-17.
+//
+
+import Testing
+@testable import AnimeTracker
+
+struct AnimeTrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
