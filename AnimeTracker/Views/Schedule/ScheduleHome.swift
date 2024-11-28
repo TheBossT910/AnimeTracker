5f3852db67360493f4d39cf64c44890ae332ad39
Taha Rashid <taharashid710192@gmail.com>
2024-11-27 23:45:37 -0500

    feat - added splash image extension data into json

---
+++ AnimeTracker/Views/Schedule/ScheduleHome.swift
@@ -0,0 +1,18 @@
+//
+//  ScheduleHome.swift
+//  AnimeTracker
+//
+//  Created by Taha Rashid on 2024-11-27.
+//
+
+import SwiftUI
+
+struct ScheduleHome: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ScheduleHome()
+}
