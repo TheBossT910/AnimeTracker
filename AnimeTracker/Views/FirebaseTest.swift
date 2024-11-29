1766bcb276bf8695619c6aa4604ecbfa3205db60
Taha Rashid <taharashid710192@gmail.com>
2024-11-28 23:54:18 -0500

    feat - added initial Firebase support
    Added files related to Firebase
    Imported Firebase dependancies
    Created the FirebaseTest view where Firebase is implemented (in testing)

---
+++ AnimeTracker/Views/FirebaseTest.swift
@@ -0,0 +1,18 @@
+//
+//  FirebaseTest.swift
+//  AnimeTracker
+//
+//  Created by Taha Rashid on 2024-11-28.
+//
+
+import SwiftUI
+
+struct FirebaseTest: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FirebaseTest()
+}
