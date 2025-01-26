5e6dbcfa52ae13d83aa4c627a7000d370f1af1e8
Taha Rashid <taharashid710192@gmail.com>
2025-01-26 12:50:19 -0500

    feat: Created ScheduleAiringRow to show multiple Schedule Airing items

---
+++ AnimeTracker/Views/Schedule/ScheduleAiringRow.swift
@@ -0,0 +1,27 @@
+//
+//  ScheduleAiringRow.swift
+//  AnimeTracker
+//
+//  Created by Taha Rashid on 2025-01-26.
+//
+
+import SwiftUI
+
+struct ScheduleAiringRow: View {
+    @EnvironmentObject var animeDataFB: AnimeDataFirebase   //stores an object to hold Firebase data
+    //Oshi no Ko, Attack on Titan, Boochi the Rock!
+    var animeAiringIDs: [String] = ["6KaHVRxICvkkrRYsDiMY", "eqIKQyLZ7eMe8GmMOB6O", "323Prp20ZeyevdQQcyl9"]
+    
+    var body: some View {
+        ForEach (animeAiringIDs, id: \.self) { animeAiringID in
+            ScheduleAiringItem(animeID: animeAiringID)
+        }
+    }
+}
+
+#Preview {
+    //environment object
+    let animeDataFB = AnimeDataFirebase(collection: "s1")
+    ScheduleAiringRow()
+        .environmentObject(animeDataFB)
+}
