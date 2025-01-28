1e4e5ed0650c04bf54a33b77af6dc5d1f2ceb698
Taha Rashid <taharashid710192@gmail.com>
2025-01-27 19:59:02 -0500

    fix: created more spacing for the category names

--- AnimeTracker/Views/Categories/CategoryRow.swift
+++ AnimeTracker/Views/Categories/CategoryRow.swift
@@ -20,7 +20,7 @@ struct CategoryRow: View {
             Text(categoryName)
                 .font(.headline)
                 .padding(.leading, 15)
-                .padding(.top, 5)
+                .padding(.top, 20)
             
             //lets us scroll horizontally. showIndicators: false means don't show a scrollbar
             ScrollView(.horizontal, showsIndicators: false) {
