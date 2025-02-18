c010ef33a1aacfcf9ae65a8b90ffafc16b84a2aa
Taha Rashid <taharashid710192@gmail.com>
2025-02-18 12:03:31 -0500

    chore: created Database class that implements DatabaseProtocol
    We still need to create the implementation

--- AnimeTracker/Models/Database.swift
+++ AnimeTracker/Models/Database.swift
@@ -4,5 +4,14 @@
 //
 //  Created by Taha Rashid on 2025-02-18.
 //
+// Updated file to communicate with the database.
+// This replaces AnimeFirebaseDatabase, which will be deprecated at a later date
+
+// TODO: Implement new database code. We are using the new database structure for Firebase. See Notion for more details
+// TODO: Create an interface (called Protocols) for all of the methods in this file. This is to make it easier in the future if we change databases
 
 import Foundation
+
+class Database : DatabaseProtocol {
+    
+}
