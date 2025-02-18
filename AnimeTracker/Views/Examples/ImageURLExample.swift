b3fa91f80ed9208e8004f4e44b42733338273fa6
Taha Rashid <taharashid710192@gmail.com>
2025-02-18 11:49:39 -0500

    chore: created ImageURLExample file

---
+++ AnimeTracker/Views/Examples/ImageURLExample.swift
@@ -0,0 +1,18 @@
+//
+//  ImageURLExample.swift
+//  AnimeTracker
+//
+//  Created by Taha Rashid on 2025-02-18.
+//
+
+import SwiftUI
+
+struct ImageURLExample: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ImageURLExample()
+}
