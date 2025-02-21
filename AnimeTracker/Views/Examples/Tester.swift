37418c563516f6596a306390748a0758e4df3213
Taha Rashid <taharashid710192@gmail.com>
2025-02-20 23:05:30 -0500

    test: added testing file

---
+++ AnimeTracker/Views/Examples/Tester.swift
@@ -0,0 +1,18 @@
+//
+//  Tester.swift
+//  AnimeTracker
+//
+//  Created by Taha Rashid on 2025-02-20.
+//
+
+import SwiftUI
+
+struct Tester: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Tester()
+}
