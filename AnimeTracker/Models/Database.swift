3852128b73bc3587795238c91177a879686c8885
Taha Rashid <taharashid710192@gmail.com>
2025-02-20 23:06:48 -0500

    feat: implemented function to grab database data
    This still needs to be optimized, but it works. I also created a tester file to show it working/test it as well

--- AnimeTracker/Models/Database.swift
+++ AnimeTracker/Models/Database.swift
@@ -11,7 +11,71 @@
 // TODO: Create an interface (called Protocols) for all of the methods in this file. This is to make it easier in the future if we change databases
 
 import Foundation
+import FirebaseFirestore
 
-class Database : DatabaseProtocol {
+@MainActor class Database : DatabaseProtocol, ObservableObject {
+    @Published var anime_data: [String: [String: [String: [String : Any]]]]
+    private var db: Firestore
     
+    // constructor
+    init() {
+        self.anime_data = [:]
+        // creating a Firestore instance
+        self.db = Firestore.firestore()
+        
+        // running async tasks
+        Task {
+            await getDocuments()
+        }
+    }
+    
+    // getting all documents from Firebase
+    func getDocuments() async {
+        do {
+            // get all documents (all animes) in anime_data
+            var animeArray: [String] = []
+            let queryAnime = try await self.db.collection("/anime_data/").getDocuments()
+            queryAnime.documents.forEach { document in
+                // append all anime IDs to array
+                let animeID = document.documentID
+                animeArray.append(animeID)
+                
+                // adding main document to array
+//                anime_data[animeID]?["main"] = document.data()
+            }
+            
+            // creating arrays to store data for their respective documents
+            var dataArray: [String: [String: Any]] = [:]
+            var episodeArray: [String: [String: Any]] = [:]
+            
+            // parsing each anime
+            var counter: Int = 0
+            for animeID in animeArray {
+                // getting all data documents
+                let queryData = try await self.db.collection("/anime_data/\(animeID)/data").getDocuments()
+                queryData.documents.forEach { document in
+                    // add each data document to array
+                    dataArray[document.documentID] = document.data()
+                }
+                
+                //getting all episode documents
+                let queryEpisodes = try await self.db.collection("/anime_data/\(animeID)/episodes").getDocuments()
+                queryEpisodes.documents.forEach { document in
+                    // add each episode document to array
+                    episodeArray[document.documentID] = document.data()
+                }
+                
+                let queryMain = try await self.db.collection("/anime_data/").document(animeID).getDocument()
+                let  mainData = queryMain.data()!
+                
+                anime_data[animeID] = ["anime": ["main": mainData], "data": dataArray, "episodes": episodeArray]
+                counter = counter + 1
+                print("\(counter)/200")
+            }
+        
+            
+        } catch {
+            print("Error fetching documents: \(error)")
+        }
+    }
 }
--- AnimeTracker/Views/Examples/Tester.swift
+++ AnimeTracker/Views/Examples/Tester.swift
@@ -7,12 +7,71 @@
 
 import SwiftUI
 
-struct Tester: View {
+struct Tester2: View {
+    @ObservedObject var dbTest: Database = Database()
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        let animeID = "163134"
+        Text(dbTest.anime_data[animeID]?["anime"]?["main"]?["title"] as? String ?? "N/A")
+//        Text(dbTest.anime_data["107372"]?["data"]?["files"]?["box_image"] as? String ?? "N/A")
+        
+        var boxImage = URL(string: dbTest.anime_data[animeID]?["data"]?["files"]?["box_image"] as? String ?? "N/A")
+        
+        // displaying the box image
+        AsyncImage(url: boxImage) { image in
+            image
+                .resizable()
+                .clipShape(RoundedRectangle(cornerRadius: 10))
+                //overlaying a rounded rectangle with a white border
+                .overlay {
+                    RoundedRectangle(cornerRadius: 10).stroke(Color.white, lineWidth: 5)
+                }
+                //adding a shadow. Gray on light mode, white on dark mode
+                .shadow(color: Color.gray.opacity(0.7), radius: 10)
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 150)
+        } placeholder: {
+            Color.blue
+            .frame(width: 150, height: 212)
+            .clipShape(RoundedRectangle(cornerRadius: 10))
+            //overlaying a rounded rectangle with a white border
+            .overlay {
+                RoundedRectangle(cornerRadius: 10).stroke(Color.white, lineWidth: 5)
+            }
+            //adding a shadow. Gray on light mode, white on dark mode
+            .shadow(color: Color.gray.opacity(0.7), radius: 10)
+        }
+        
+        // displaying the splash image
+        var splashImage = URL(string: dbTest.anime_data[animeID]?["data"]?["files"]?["splash_image"] as? String ?? "N/A")
+        
+        
+        AsyncImage(url: splashImage) { image in
+            image
+                .resizable()
+                .clipShape(RoundedRectangle(cornerRadius: 10))
+                //overlaying a rounded rectangle with a white border
+                .overlay {
+                    RoundedRectangle(cornerRadius: 10).stroke(Color.white, lineWidth: 5)
+                }
+                //adding a shadow. Gray on light mode, white on dark mode
+                .shadow(color: Color.gray.opacity(0.7), radius: 10)
+                .aspectRatio(contentMode: .fit)
+                .frame(height: 80)
+        } placeholder: {
+            Color.blue
+            .frame(width: 150, height: 212)
+            .clipShape(RoundedRectangle(cornerRadius: 10))
+            //overlaying a rounded rectangle with a white border
+            .overlay {
+                RoundedRectangle(cornerRadius: 10).stroke(Color.white, lineWidth: 5)
+            }
+            //adding a shadow. Gray on light mode, white on dark mode
+            .shadow(color: Color.gray.opacity(0.7), radius: 10)
+        }
+        
     }
 }
 
 #Preview {
-    Tester()
+    Tester2()
 }
