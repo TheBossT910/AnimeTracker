0966cb8e21d9b70ac1284ca145389f02fe68d99d
Taha Rashid <taharashid710192@gmail.com>
2025-03-08 23:23:34 -0500

    feat: initial implementation of ShowCard view
    Still need to work on it

--- AnimeTracker/Glassmorphic Designs/ShowCard.swift
+++ AnimeTracker/Glassmorphic Designs/ShowCard.swift
@@ -8,8 +8,37 @@
 import SwiftUI
 
 struct ShowCard: View {
+    var width: CGFloat = 168
+    var height: CGFloat = 276
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            BasicCard(width: width, height: height, radius: 12)
+            VStack(alignment: .center) {
+                Spacer()
+    
+                VStack(alignment: .leading) {
+                    // title
+                    Spacer()
+                    Text("SOLO LEVELING: REAWAKENED")
+                        .font(.headline)
+                        .fontWeight(.semibold)
+                }
+                
+                Spacer()
+                
+                // buttons
+                HStack(alignment: .top) {
+                    // details button
+                    BasicCard(width: 70, height: 31, radius: 5)
+                    // watch now button
+                    BasicCard(width: 70, height: 57, radius: 10)
+                }
+                .padding()
+            }
+            .frame(width: width, height: height)
+        }
+        
     }
 }
 
