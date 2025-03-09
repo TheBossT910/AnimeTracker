1740c58a2c28a07346ee423595844423c0a59903
Taha Rashid <taharashid710192@gmail.com>
2025-03-08 23:22:23 -0500

    chore: created/renamed design files

---
+++ AnimeTracker/Glassmorphic Designs/BasicCard.swift
@@ -0,0 +1,18 @@
+//
+//  BasicCard.swift
+//  AnimeTracker
+//
+//  Created by Taha Rashid on 2025-03-08.
+//
+
+import SwiftUI
+
+struct BasicCard: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BasicCard()
+}
