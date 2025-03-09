7c01902a9f374aea4d5de4668ef47290a897b19e
Taha Rashid <taharashid710192@gmail.com>
2025-03-08 23:23:14 -0500

    feat: created BasicCard view
    This will be the base for all card views

--- AnimeTracker/Glassmorphic Designs/BasicCard.swift
+++ AnimeTracker/Glassmorphic Designs/BasicCard.swift
@@ -8,11 +8,29 @@
 import SwiftUI
 
 struct BasicCard: View {
+    
+    // default values
+    var width: CGFloat = 300
+    var height: CGFloat = 500
+    var radius: CGFloat = 20
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        // glassmorphic card
+        RoundedRectangle(cornerRadius: radius, style: .continuous)
+            .fill(Color.white.opacity(0.5))
+            .background {
+                RoundedRectangle(cornerRadius: radius, style: .continuous)
+                    .stroke(.gray.opacity(0.3), lineWidth: 15)
+                    .blur(radius: radius / 2)
+            }
+            .background(.thinMaterial.opacity(0.90))
+            .frame(width: width, height: height)
+            // TODO: This is causing user to see rectange corners
+
+//            .shadow(color: Color.gray.opacity(0.3), radius: 5)
     }
 }
 
 #Preview {
-    BasicCard()
+    BasicCard(width: 300, height: 500, radius: 20)
 }
