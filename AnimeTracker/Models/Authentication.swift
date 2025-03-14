ac700f72560e09734ede550134948d6d9eff641f
Taha Rashid <taharashid710192@gmail.com>
2025-03-14 19:31:42 -0400

    chore: various file operations

--- AnimeTracker/Models/Authentication.swift
+++
@@ -1,8 +0,0 @@
-//
-//  Authentication.swift
-//  AnimeTracker
-//
-//  Created by Taha Rashid on 2025-03-14.
-//
-
-import Foundation
--- AnimeTracker/Models/AuthenticationStructure.swift
+++
@@ -1,8 +0,0 @@
-//
-//  AuthenticationStructure.swift
-//  AnimeTracker
-//
-//  Created by Taha Rashid on 2025-03-14.
-//
-
-import Foundation
