9030c4234996c515610327a3eb5bd3aecbfab524
Taha Rashid <taharashid710192@gmail.com>
2025-03-14 19:31:20 -0400

    feat: created AuthView view (still work-in-progress)

--- AnimeTracker/Views/AuthView.swift
+++ AnimeTracker/Views/AuthView.swift
@@ -8,8 +8,30 @@
 import SwiftUI
 
 struct AuthView: View {
+    @State private var username: String = ""
+    @State private var password: String = ""
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text("AnimeTracker Login Page")
+        TextField("Username", text: $username)
+            .textFieldStyle(RoundedBorderTextFieldStyle())
+            .padding()
+        SecureField("Password", text: $password)
+            .onSubmit {
+                //... auto sign-in
+            }
+            .textFieldStyle(RoundedBorderTextFieldStyle())
+            .padding(.leading)
+        
+        // explicit buttons
+        Button("Sign in") {
+            //...
+        }
+        .padding(.bottom)
+        
+        Button("Sign up") {
+            //...
+        }
     }
 }
 
