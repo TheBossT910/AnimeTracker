4006dca474fbe737f2ccc298d5ed0d93f536c5c0
Taha Rashid <taharashid710192@gmail.com>
2025-03-15 18:19:45 -0400

    fix: renamed items

--- AnimeTracker/Views/Helper/WatchlistMenu.swift
+++ AnimeTracker/Views/Helper/WatchlistMenu.swift
@@ -1,5 +1,5 @@
 //
-//  WachlistMenu.swift
+//  WatchlistMenu.swift
 //  AnimeTracker
 //
 //  Created by Taha Rashid on 2025-03-15.
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-struct WachlistMenu: View {
+struct WatchlistMenu: View {
     @EnvironmentObject var db: Database
     @EnvironmentObject var authManager: AuthManager
     
@@ -21,44 +21,47 @@ struct WachlistMenu: View {
     @State var isPlanToWatch = false
     
     var body: some View {
-        Menu {
-            Button(action: {
-                isDropped.toggle()
-                let watchlist = db.userData[userID]?.dropped ?? []
-                db.toggleWatchlist(userID: userID, animeID: Int(animeID) ?? -1, watchlist: watchlist, watchlistName: "dropped")
-            }) {
-                Label("Dropped", systemImage: isDropped ? "folder" : "folder.fill")
+        let userData = db.userData[userID]
+        if (userData != nil) {
+            Menu {
+                Button(action: {
+                    isDropped.toggle()
+                    let watchlist = userData?.dropped ?? []
+                    db.toggleWatchlist(userID: userID, animeID: Int(animeID) ?? -1, watchlist: watchlist, watchlistName: "dropped")
+                }) {
+                    Label("Dropped", systemImage: isDropped ? "bookmark.fill" : "bookmark")
+                }
+                Button(action: {
+                    isCompleted.toggle()
+                    let watchlist = userData?.completed ?? []
+                    db.toggleWatchlist(userID: userID, animeID: Int(animeID) ?? -1, watchlist: watchlist, watchlistName: "completed")
+                }) {
+                    Label("Completed", systemImage: isCompleted ? "bookmark.fill" : "bookmark")
+                }
+                Button(action: {
+                    isWatching.toggle()
+                    let watchlist = userData?.watching ?? []
+                    db.toggleWatchlist(userID: userID, animeID: Int(animeID) ?? -1, watchlist: watchlist, watchlistName: "watching")
+                }) {
+                    Label("Watching", systemImage: isWatching ? "bookmark.fill" : "bookmark")
+                }
+                Button(action: {
+                    isPlanToWatch.toggle()
+                    let watchlist = userData?.plan_to_watch ?? []
+                    db.toggleWatchlist(userID: userID, animeID: Int(animeID) ?? -1, watchlist: watchlist, watchlistName: "plan_to_watch")
+                }) {
+                    Label("Plan to Watch", systemImage: isPlanToWatch ? "bookmark.fill" : "bookmark")
+                }
+            } label: {
+                Label("Add to Watchlist", systemImage: "list.bullet.rectangle.portrait")
             }
-            Button(action: {
-                isCompleted.toggle()
-                let watchlist = db.userData[userID]?.completed ?? []
-                db.toggleWatchlist(userID: userID, animeID: Int(animeID) ?? -1, watchlist: watchlist, watchlistName: "completed")
-            }) {
-                Label("Completed", systemImage: isCompleted ? "folder" : "folder.fill")
+            // initially setting values
+            .onAppear {
+                isDropped = watchlists["Dropped"] ?? false
+                isWatching = watchlists["Watching"] ?? false
+                isCompleted = watchlists["Completed"] ?? false
+                isPlanToWatch = watchlists["Plan to Watch"] ?? false
             }
-            Button(action: {
-                isWatching.toggle()
-                let watchlist = db.userData[userID]?.watching ?? []
-                db.toggleWatchlist(userID: userID, animeID: Int(animeID) ?? -1, watchlist: watchlist, watchlistName: "watching")
-            }) {
-                Label("Watching", systemImage: isWatching ? "folder" : "folder.fill")
-            }
-            Button(action: {
-                isPlanToWatch.toggle()
-                let watchlist = db.userData[userID]?.plan_to_watch ?? []
-                db.toggleWatchlist(userID: userID, animeID: Int(animeID) ?? -1, watchlist: watchlist, watchlistName: "plan_to_watch")
-            }) {
-                Label("Plan to Watch", systemImage: isPlanToWatch ? "folder" : "folder.fill")
-            }
-        } label: {
-            Label("Add to Watchlist", systemImage: "star")
-        }
-        // initially setting values
-        .onAppear {
-            isDropped = watchlists["Dropped"] ?? false
-            isWatching = watchlists["Watching"] ?? false
-            isCompleted = watchlists["Completed"] ?? false
-            isPlanToWatch = watchlists["Plan to Watch"] ?? false
         }
     }
 }
@@ -68,7 +71,7 @@ struct WachlistMenu: View {
     let db = Database()
     let authManager = AuthManager.shared
     
-    WachlistMenu(animeID: "163134", userID: "zZ81pmI0bQcU7a6NsocUn7A4owU2", watchlists: ["Dropped": false, "Watching": true, "Completed": false, "Plan to Watch": true])
+    WatchlistMenu(animeID: "163134", userID: "zZ81pmI0bQcU7a6NsocUn7A4owU2", watchlists: ["Dropped": false, "Watching": true, "Completed": false, "Plan to Watch": true])
         .environmentObject(db)
         .environmentObject(authManager)
 }
