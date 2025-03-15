c6f7a174cbc576c7cf46573ebcfd94d2d7c5cf79
Taha Rashid <taharashid710192@gmail.com>
2025-03-15 02:48:16 -0400

    feat: added new @EnvironmentObject vars, and AuthView as a page

--- AnimeTracker/Views/ContentView.swift
+++ AnimeTracker/Views/ContentView.swift
@@ -15,6 +15,7 @@ struct ContentView: View {
         case animeStatus
         case list
         case schedule
+        case profile
     }
     
     var body: some View {
@@ -41,6 +42,11 @@ struct ContentView: View {
                     Label("Schedule", systemImage: "calendar")
                 }
                 .tag(Tab.schedule)
+            AuthView()
+                .tabItem {
+                    Label("Account", systemImage: "person.crop.circle")
+                }
+                .tag(Tab.profile)
         }
     }
 }
@@ -48,6 +54,11 @@ struct ContentView: View {
 #Preview {
     //environment objects
     var animeDataFB = AnimeDataFirebase(collection: "s1")
+    let db = Database()
+    let authManager = AuthManager.shared
+    
     ContentView()
         .environmentObject(animeDataFB)
+        .environmentObject(db)
+        .environmentObject(authManager)
 }
