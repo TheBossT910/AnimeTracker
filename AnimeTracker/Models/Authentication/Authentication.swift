694b857b205be225c18e96b77c0d0a224671798e
Taha Rashid <taharashid710192@gmail.com>
2025-03-14 22:20:39 -0400

    feat: fully implemented working Authentication

--- AnimeTracker/Models/Authentication/Authentication.swift
+++ AnimeTracker/Models/Authentication/Authentication.swift
@@ -6,14 +6,24 @@
 //
 
 import Foundation
+import FirebaseFirestore
 import FirebaseAuth
 
+// TODO: throw errors for the view!
 class AuthManager: ObservableObject {
     static let shared = AuthManager()       // singleton instance
     @Published var userID: String? = nil    // store user id
+    @Published var userName: String? = nil  // user name
+    @Published var isAuthenticated: Bool = false    // if user is signed in
+    private var db: Firestore
     
     init() {
-        self.userID = Auth.auth().currentUser?.uid  // gets the current logged-in user, if any
+        self.db = Firestore.firestore()
+        if let userID = Auth.auth().currentUser?.uid {
+            self.userID = userID  // gets the current logged-in user, if any
+            self.isAuthenticated = true
+            self.getUserName(userID: userID)
+        }
     }
     
     func signIn(email: String, password: String) {
@@ -21,26 +31,46 @@ class AuthManager: ObservableObject {
             // try to get the user's id
             if let user = authResult?.user {
                 self.userID = user.uid
+                self.isAuthenticated = true
+                self.getUserName(userID: user.uid)
+//                print("user signed in!")
             }
         }
     }
     
-    func signOut() throws {
+    func signOut() {
         // sign out the current user
         do {
             try Auth.auth().signOut()
             self.userID = nil
+            self.userName = nil
+            self.isAuthenticated = false
         } catch {
             print("Error signing user out: \(error.localizedDescription)")
         }
     }
     
-    func signUp(email: String, password: String) {
+    func signUp(email: String, password: String, username: String) {
         Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
             // grab the user's id (they are also currently signed-in now)
             if let user = authResult?.user {
                 self.userID = user.uid
+                self.isAuthenticated = true
+                self.userName = username
+//                print("user signed up!")
+                
+                // add a new user data entry into database
+                self.db.collection("/user_data/").document(user.uid).setData([
+                    "favorites": [],
+                    "user_name": username
+                ])
             }
+        }
+    }
+    
+    func getUserName(userID: String) {
+        Task {
+            self.userName = try await self.db.collection("/user_data/").document(userID).getDocument().data()?["user_name"] as? String ?? "None"
         }
     }
 }
--- AnimeTracker/Views/AuthView.swift
+++ AnimeTracker/Views/AuthView.swift
@@ -8,29 +8,99 @@
 import SwiftUI
 
 struct AuthView: View {
-    @State private var username: String = ""
+    @StateObject private var authManager = AuthManager.shared
+    @State private var email: String = ""
     @State private var password: String = ""
-    
+    @State private var username: String = ""
+    @State private var signUpMode: Bool = false
+
     var body: some View {
-        Text("AnimeTracker Login Page")
-        TextField("Username", text: $username)
-            .textFieldStyle(RoundedBorderTextFieldStyle())
-            .padding()
-        SecureField("Password", text: $password)
-            .onSubmit {
-                //... auto sign-in
+        
+        if (!authManager.isAuthenticated && !signUpMode) {
+            Text("Welcome to **your** AnimeTracker")
+                .font(.largeTitle)
+                .fontWeight(.medium)
+                
+            TextField("Email", text: $email)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .autocorrectionDisabled()
+                .textInputAutocapitalization(.never)
+                .padding()
+                
+            SecureField("Password", text: $password)
+                .onSubmit {
+                    // auto sign-in
+                    authManager.signIn(email: email, password: password)
+                }
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .padding(.leading)
+            
+            // explicit buttons
+            Button("Sign in") {
+                authManager.signIn(email: email, password: password)
             }
-            .textFieldStyle(RoundedBorderTextFieldStyle())
-            .padding(.leading)
-        
-        // explicit buttons
-        Button("Sign in") {
-            //...
+            .disabled(email == "" || (password.count < 6) || password == "")
+            .padding()
+            .font(.title2)
+            
+            Button("Sign up") {
+                signUpMode = true
+            }
         }
-        .padding(.bottom)
         
-        Button("Sign up") {
-            //...
+        else if (signUpMode) {
+            Text("Create an account!")
+                .font(.title)
+            
+            TextField("Username", text: $username)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .autocorrectionDisabled()
+                .textInputAutocapitalization(.never)
+                .padding(.leading)
+            Text("Must **create a username**.")
+                .font(.caption)
+                .padding(.bottom)
+            
+            TextField("Email", text: $email)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .autocorrectionDisabled()
+                .textInputAutocapitalization(.never)
+                .padding(.leading)
+            Text("Must use a **valid email address**. i.e. example@anime.com")
+                .font(.caption)
+                .padding(.bottom)
+                
+            SecureField("Password", text: $password)
+                .onSubmit {
+                    // auto sign-in
+                    authManager.signIn(email: email, password: password)
+                }
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .padding(.leading)
+            Text("Passwords must have **6+ characters**.")
+                .font(.caption)
+            
+            Button("Sign up") {
+                authManager.signUp(email: email, password: password, username: username)
+                signUpMode = false
+            }
+            .disabled(email == "" || (password.count < 6) || password == "" || username == "")
+        }
+            
+        
+        else {
+            Text("Authenticated!")
+            Text ("Welcome back, \(authManager.userName ?? "None")")
+                .padding()
+            
+            //sign out button
+            Button("Sign out") {
+                authManager.signOut()
+                email = ""
+                password = ""
+                username = ""
+            }
+            .tint(Color.red)
         }
     }
 }
