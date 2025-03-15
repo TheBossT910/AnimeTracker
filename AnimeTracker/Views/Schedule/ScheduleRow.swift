0b1097f89a006e41e1aaa1df2b73237ad46f3970
Taha Rashid <taharashid710192@gmail.com>
2025-03-15 02:21:37 -0400

    feat: implemented with new Database and AuthManager classes

--- AnimeTracker/Views/Schedule/ScheduleRow.swift
+++ AnimeTracker/Views/Schedule/ScheduleRow.swift
@@ -8,18 +8,22 @@
 //TODO: Add the "top shows" section
 
 import SwiftUI
+import Foundation
 
 struct ScheduleRow: View {
-    @EnvironmentObject var animeDataFB: AnimeDataFirebase  //holds a AnimeDataFirebase object, with Firebase data
+    @EnvironmentObject var db: Database
+    @EnvironmentObject var authManager: AuthManager
+    
     var day: String
     
-    var filteredAnimes: [String : [String: Any]] {
-        animeDataFB.animes.filter { anime in
-            let curGeneral = anime.value["general"] as? general?
-            //TODO: Seperate broadcast day and time so we don't have to process the combined String
-            //NOTE: curDay has an "s" after every day!
-            let curDay = curGeneral??.broadcast?.split(separator: " ")[0]
-            return String(curDay ?? "N/A") == day
+    var filteredAnimes: [String : anime] {
+        db.animeNew.filter { anime in
+            // TODO: We are only basing the air date off of the 1st show... Fix this!
+            let latestEpisode = anime.value.episodes?.first
+            let broadcastUnix: TimeInterval = Double(latestEpisode?.broadcast ?? 0)
+            let weekday = getWeekday(from: broadcastUnix)
+            
+            return (weekday + "s") == day
         }
     }
 
@@ -37,31 +41,42 @@ struct ScheduleRow: View {
             Text(animeKeys.isEmpty ? "No Animes Found" : "")
                 .font(.headline)
             
-            //displays each splash image... using normal image for now
+            // display each airing show
             ForEach(animeKeys, id: \.self) { animeKey in
-                //grabbing splash image 
-                let animeFB = animeDataFB.animes[animeKey]
-                let curFiles = animeFB?["files"] as? files?
-                let splashImage = curFiles??.splash_image ?? "N/A"
                 
                 //NavigationLink allows us to navigate to AnimeDetail when clicked (when importing this view into another view)
                 NavigationLink {
                     AnimeDetail(animeID: animeKey)
                 } label: {
-                    ScheduleItem(animeID: animeKey, splashImage: Image(splashImage))
+                    ScheduleItem(animeID: animeKey)
                 }
                 //fixes the bug where all text is highlighted blue in views that use this view
                 .buttonStyle(.plain)
             }
         }
+    }
 
+    // function courtesy of ChatGPT.
+    // TODO: create your own implementation
+    func getWeekday(from unixTimestamp: TimeInterval) -> String {
+        let date = Date(timeIntervalSince1970: unixTimestamp)
+        
+        let formatter = DateFormatter()
+        formatter.dateFormat = "EEEE"  // Full weekday name (e.g., "Monday")
+        formatter.timeZone = TimeZone.current  // Uses the device's timezone
+        
+        return formatter.string(from: date)
     }
 }
 
 #Preview {
     //environment object
+    let db = Database()
+    let authManager = AuthManager.shared
     var animeDataFB = AnimeDataFirebase(collection: "s1")
     
     ScheduleRow(day: "Tuesdays")
         .environmentObject(animeDataFB)
+        .environmentObject(db)
+        .environmentObject(authManager)
 }
