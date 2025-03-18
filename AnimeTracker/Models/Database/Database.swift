0e5eda8afcbcc3c7eb70aacfa8d71ef274fb2fc3
Taha Rashid <taharashid710192@gmail.com>
2025-03-17 22:49:03 -0400

    feat: implemented getInitialDocuments(), getNextDocuments(), getMarkedDocuments()
    getInitialDocuments() and getNextDocuments() are just cleaner implementations using getDocuments() as a separate method itself. getMarkedDocuments() gets all favorited/watchlisted shows immediately when requested, and is only initially loaded once

--- AnimeTracker/Models/Database/Database.swift
+++ AnimeTracker/Models/Database/Database.swift
@@ -21,6 +21,7 @@ import FirebaseFirestore
     @Published var userData: [String : user_data]
     private var db: Firestore
     private var lastDocumentSnapshot: DocumentSnapshot?
+    private var gotMarkedDocuments: Bool = false
     
     // constructor
     init() {
@@ -34,35 +35,18 @@ import FirebaseFirestore
         
         // running async tasks
         Task {
-            await getDocuments()
+            await getInitialDocuments(documentAmount: 20)
             await getUserDocuments()
         }
     }
     
-    // getting all documents from Firebase (async)
-    func getDocuments() async {
-        // getting all anime ids
-        var animeIDs : [String] = []
-        do {
-            // fetch all anime documents
-            let queryAnime = try await self.db.collection("/anime_data").limit(to: 20).getDocuments()
-            var counter = 0
-            queryAnime.documents.forEach { document in
-                // add to array
-                animeIDs.append(document.documentID)
-                counter += 1
-            }
-            print("# of anime documents: \(counter)")
-            lastDocumentSnapshot = queryAnime.documents.last
-            print(lastDocumentSnapshot.debugDescription)
-        }
-        catch {
-            print("Error fetching documents: \(error)")
-        }
+    // retrieves documents from Firebase (async)
+    private func getDocuments(animeIDs: [String]) async -> [String: anime] {
+        // storing async response in 'response' variable
+        var response: [String: anime] = [:]
         
+        // try to get anime data
         do {
-            // storing async response in 'response' variable
-            var response: [String: anime] = [:]
             response = try await withThrowingTaskGroup(of: (String, anime).self) { group in
                 // going through each animeID
                 for animeID in animeIDs {
@@ -104,23 +88,22 @@ import FirebaseFirestore
                 // return parsed data (to response)
                 return animes
             }
-            
-            // set anime to response data
-            animeData = response
-            
         } catch {
             print("Error getting documents: \(error)")
         }
+        
+        // return response data
+        return response
     }
     
-    func getNextDocuments() async {
+    // retrives the initial batch of animes from database
+    private func getInitialDocuments(documentAmount: Int = 20) async {
         // getting all anime ids
         var animeIDs : [String] = []
         do {
             // fetch all anime documents
             let queryAnime = try await self.db.collection("/anime_data")
-                .start(afterDocument: self.lastDocumentSnapshot!)
-                .limit(to: 20)
+                .limit(to: documentAmount)
                 .getDocuments()
             var counter = 0
             queryAnime.documents.forEach { document in
@@ -128,67 +111,77 @@ import FirebaseFirestore
                 animeIDs.append(document.documentID)
                 counter += 1
             }
-            print("# of added anime documents: \(counter)")
+            print("Initial anime documents: \(counter)")
             lastDocumentSnapshot = queryAnime.documents.last
         }
         catch {
             print("Error fetching documents: \(error)")
         }
         
+        let response = await getDocuments(animeIDs: animeIDs)
+        // set animeData to response
+        animeData = response
+    }
+
+    // retrieve the next animes from database
+    public func getNextDocuments(documentAmount: Int = 20) async {
+        // getting all anime ids
+        var animeIDs : [String] = []
         do {
-            // storing async response in 'response' variable
-            var response: [String: anime] = [:]
-            response = try await withThrowingTaskGroup(of: (String, anime).self) { group in
-                // going through each animeID
-                for animeID in animeIDs {
-                    // creating a task for each anime
-                    group.addTask {
-                        // for main
-                        let currentMain = try await self.db.collection("/anime_data/").document(animeID).getDocument(as: main.self)
-                        
-                        // for data
-                        let currentFile = try await self.db.collection("/anime_data/\(animeID)/data").document("files").getDocument(as: files.self)
-                        let currentGeneral = try await self.db.collection("/anime_data/\(animeID)/data").document("general").getDocument(as: general.self)
-                        
-                        // for episodes
-                        var currentEpisodes: [episodes] = []
-                        let queryEpisodes = try await self.db.collection("/anime_data/\(animeID)/episodes").getDocuments()
-                        queryEpisodes.documents.forEach { document in
-                            currentEpisodes.append(try! document.data(as: episodes.self))
-                        }
-                        
-                        // creating anime object
-                        let currentAnime: anime = anime(
-                            id: animeID,
-                            main: currentMain,
-                            data: data(files: currentFile, general: currentGeneral),
-                            episodes: currentEpisodes
-                        )
-                        
-                        return (animeID, currentAnime)
-                        }
-                    }
-                
-                // parsing data
-                var animes = [String: anime]()
-                for try await (animeID, currentAnime) in group {
-                    // adding data to proper spots in array
-                    animes[animeID] = currentAnime
-                }
-                
-                // return parsed data (to response)
-                return animes
+            // fetch all anime documents
+            let queryAnime = try await self.db.collection("/anime_data")
+                .start(afterDocument: self.lastDocumentSnapshot!)
+                .limit(to: documentAmount)
+                .getDocuments()
+            var counter = 0
+            queryAnime.documents.forEach { document in
+                // add to array
+                animeIDs.append(document.documentID)
+                counter += 1
             }
+            print("Added anime documents: \(counter)")
+            lastDocumentSnapshot = queryAnime.documents.last
+        }
+        catch {
+            print("Error fetching documents: \(error)")
+        }
+        
+        let response = await getDocuments(animeIDs: animeIDs)
+        // merge previous anime data with new response data
+        animeData.merge(response) { (_, new) in new }
+    }
+    
+    // immediately retrieves all favorited/watchlisted shows
+    public func getMarkedDocuments(userID: String) async {
+        if (!gotMarkedDocuments) {
+            // getting all different data arrays
+            let favorites = userData[userID]!.favorites!
+            let completed = userData[userID]!.completed!
+            let dropped = userData[userID]!.dropped!
+            let watching = userData[userID]!.watching!
+            let planToWatch = userData[userID]!.plan_to_watch!
             
-            // set anime to response data
+            // merging into 1 array
+            // this creates 1 array which has all data, then makes it into a Set (to get rid of duplicates) and converts it back into an array
+            let mergedInt = Array(Set(favorites + completed + dropped + watching + planToWatch))
+            // converts all items (Int) into Strings
+            let mergedString = mergedInt.map { String($0) }
+            
+            // getting animes
+            let animeIDs: [String] = mergedString
+            let response = await getDocuments(animeIDs: animeIDs)
+            
+            // merge previous anime data with new response data
             animeData.merge(response) { (_, new) in new }
             
-        } catch {
-            print("Error getting documents: \(error)")
+            // this variable ensures we only run this function once!
+            gotMarkedDocuments = true
+            print("Ran getMarkedDocuments!")
         }
     }
     
-    func getUserDocuments() async {
+    // retrieves user data from database
+    private func getUserDocuments() async {
         do {
             let queryUsers = try await db.collection("/user_data/").getDocuments()
             try queryUsers.documents.forEach { document in
@@ -203,7 +196,8 @@ import FirebaseFirestore
     
     // this function updates user-defined variables
     // currently, it only updates the favorite button
-    func updateFavorite(userID: String, isFavorite: Bool, animeID: Int) {
+    // toggles the given anime as a favorite
+    public func updateFavorite(userID: String, isFavorite: Bool, animeID: Int) {
         var myUser: user_data = userData[userID]!
         
         // not in favorites and we want to add to favorites
@@ -227,7 +221,8 @@ import FirebaseFirestore
     }
     
     // TODO: merge toggleWatchlist and updateFavorites into 1 function!
-    func toggleWatchlist(userID: String, animeID: Int, watchlist: [Int], watchlistName: String) {
+    // toggles a given anime from a watchlist
+    public func toggleWatchlist(userID: String, animeID: Int, watchlist: [Int], watchlistName: String) {
         var updatedWatchlist: [Int] = []
         if (watchlist.contains(animeID)) {
             // remove show
